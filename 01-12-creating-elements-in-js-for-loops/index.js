ebc857497db74cedc344d9f878e57808d1c832f1
xaviguasch <xaviguasch@gmail.com>
2020-07-13 14:38:50 +0200

    added solution to 12. creating elements - foor loops

---
+++ 01-12-creating-elements-in-js-for-loops/index.js
@@ -0,0 +1,20 @@
+var powerRangers = [
+  'Jason Lee Scott',
+  'Kimberly Hart',
+  'Zack Taylor',
+  'Trini Kwan',
+  'Billy Cranston',
+]
+
+var rangersList = document.getElementById('rangers')
+// var newName = document.createElement('li')
+// newName.textContent = powerRangers[0]
+// rangersList.append(newName)
+
+for (let i = 0; i < powerRangers.length; i++) {
+  let newName = document.createElement('li')
+  newName.textContent = powerRangers[i]
+  rangersList.append(newName)
+}
+
+console.log('hey')
