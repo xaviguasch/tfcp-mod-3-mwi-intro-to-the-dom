92af49f5e22bc4ef51fe212559b7694091cd3e41
xaviguasch <xaviguasch@gmail.com>
2020-07-13 20:44:50 +0200

    added solution to challenge 14

--- 02-14-get-the-value-from-an-input-element/index.js
+++ 02-14-get-the-value-from-an-input-element/index.js
@@ -1,1 +1,8 @@
-console.log('rhcp')
+const btn = document.querySelector('#btn')
+const input = document.querySelector('input')
+
+btn.addEventListener('click', () => {
+  const p = document.createElement('p')
+  p.textContent = input.value
+  document.body.append(p)
+})
